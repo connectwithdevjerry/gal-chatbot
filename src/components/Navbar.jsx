4628ff2b13db397e9521df455c1835c7431a9f95
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2025-01-12 03:13:18 +0100

    just commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,18 @@ import logo from "../assets/logo.svg";
 import hamburger from "../assets/hamburger.svg";
 import { HOME } from "../paths";
 import { Link, Links } from "react-router-dom";
+import bgImage from "../assets/bgImage.png";
 
 const Navbar = () => {
   return (
-    <nav className="navbar w-full px-5 py-0 navbar-expand-lg navbar-light z-50 bg-transparent fixed left-0 right-0 top-0 z-50">
+    <nav
+      className="navbar w-full px-5 py-3 navbar-expand-lg navbar-light z-50 darker fixed left-0 right-0 top-0 z-50"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <div className="flex justify-between">
         <div className="flex items-center">
           <div>
@@ -14,7 +22,9 @@ const Navbar = () => {
           </div>
           <div className="ml-4">
             <p className="font-bold text-white">Hey, I’m Erni</p>
-            <p className="text-slate-400 text-sm">your friendly earning buddy!</p>
+            <p className="text-slate-400 text-sm">
+              your friendly earning buddy!
+            </p>
           </div>
         </div>
         <div className="flex align-center w-10">
