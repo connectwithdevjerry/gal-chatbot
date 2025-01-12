dd4a6a7079a1cf79c5dcf426ee38d22b89b8dd43
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2025-01-12 05:45:37 +0100

    just commit

--- src/components/ChatItem.jsx
+++ src/components/ChatItem.jsx
@@ -1,10 +1,27 @@
-const ChatItem = ({ message, me }) => {
+import { useState, useEffect } from "react";
+
+const ChatItem = ({ message, me, speed = 150 }) => {
+  const [displayedText, setDisplayedText] = useState("");
   const itemStyle =
     "purple flex flex-wrap p-3 rounded-t-2xl rounded-bl-2xl text-white w-fit max-w-64";
+
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      setDisplayedText((prevText) => prevText + message[index]);
+      index += 1;
+      if (index === message.length) {
+        clearInterval(interval);
+      }
+    }, speed);
+
+    return () => clearInterval(interval);
+  }, [message, speed]);
+
   return (
     <div
-      dangerouslySetInnerHTML={{ __html: message }}
       className={me ? itemStyle + " ml-auto darkest" : itemStyle}
+      dangerouslySetInnerHTML={{ __html: displayedText }}
     />
   );
 };
