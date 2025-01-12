0d0f40e2d21aa64c1aef4918f314c615c20879aa
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2025-01-12 03:33:19 +0100

    just commit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <section className="dots-container mr-auto">
+    <section className="dots-container mr-auto ml-2">
       <div className="dot"></div>
       <div className="dot"></div>
       <div className="dot"></div>
